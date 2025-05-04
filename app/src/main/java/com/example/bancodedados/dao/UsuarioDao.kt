a94db6f4f1163c1ff4e9679b9be6b657a908a7b5
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-05-04 11:01:26 -0300

    feat: add update and remove functions to UsuarioDAO

--- app/src/main/java/com/example/bancodedados/dao/UsuarioDao.kt
+++ app/src/main/java/com/example/bancodedados/dao/UsuarioDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.Query
 import com.example.bancodedados.model.Usuario
 
+
 @Dao
 interface UsuarioDao {
 
@@ -13,6 +14,22 @@ interface UsuarioDao {
 
     @Query("SELECT * FROM usuarios ORDER BY uid ASC")
     fun get(): MutableList<Usuario>
+
+    @Query("UPDATE usuarios SET nome = :novoNome, idade = :novaIdade, peso = :novoPeso, altura = :novaAltura, imc = :novoIMC, classificacao = :novaClass " +
+            "WHERE UID = :id")
+
+    fun atualizar(
+        id: Int,
+        novoNome: String,
+        novaIdade: String,
+        novoPeso: String,
+        novaAltura: String,
+        novoIMC: String,
+        novaClass: String
+    )
+
+    @Query("DELETE FROM usuarios WHERE uid = :id")
+    fun deletar(id: Int)
 
     @Query("DELETE FROM usuarios")
     fun deletarTodos()
